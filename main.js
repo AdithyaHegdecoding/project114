76d5e4cc90968a51775f051225fa34c595480c70
AdithyaHegdecoding <143863709+AdithyaHegdecoding@users.noreply.github.com>
2024-06-02 11:45:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300 , 300);
+    canvas.position(500 , 300);
+}
+function draw(){
+
+}
+function take_snapshot(){
+    save("mustache.png");
+
+}
